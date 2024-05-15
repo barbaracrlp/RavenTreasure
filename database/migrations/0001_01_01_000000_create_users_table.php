d79179d5b97f95d59bb449be7eb9835e600c7935
barbaracrlp <barbaracrlp99@gmail.com>
2024-05-15 19:41:17 +0200

    primeros cambios en la tabbla user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,11 +14,17 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
+            $table->string('surname1');
+            $table->string('surname2')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('password');
+            $table->string('phone');
+            $table->boolean('logged_in');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps();//aqui esta el created_at y el updated_at
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
